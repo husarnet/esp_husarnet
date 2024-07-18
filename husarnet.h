4cd0cef1cca91867260c6e3d3c68cc29580e51bd
Milosz Lagan <milosz.lagan@husarion.com>
2024-07-18 16:30:55 +0200

    Add Arduino target architecture check

--- husarnet.h
+++ husarnet.h
@@ -3,4 +3,8 @@
 // User-facing API is defined in the
 // "husarnet/port/esp32/user_interface.h" header
 // in the included Husarnet submodule.
-#include "user_interface.h"
+#include "husarnet/ports/esp32/user_interface.h"
+
+#if defined(ARDUINO) && !defined(ARDUINO_ARCH_ESP32)
+  #error "This library only supports boards from the ESP32 family."
+#endif
